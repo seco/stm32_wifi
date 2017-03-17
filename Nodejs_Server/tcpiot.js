5334c009f4ad43208b84990337988dc3b95e73f3
zzes <kuanglong0312@sina.com>
2017-03-17 14:58:59 +0800

    server

--- Nodejs_Server/tcpiot.js
+++ Nodejs_Server/tcpiot.js
@@ -1,11 +1,17 @@
 var net = require('net')
+ 
 net.createServer(function(socket){
 	socket.on('data',function(data){
 		console.log('got:',data.toString());
-		socket.write('test on')
+
+		var text = JSON.parse(data.toString());
+		console.log(text);
+		console.log(text.tem)
 	});
 	socket.on('end',function(data){
 		console.log('end');
 	});
 	socket.write('Ready to receive your message!')
+	 
 }).listen(4001);
+
