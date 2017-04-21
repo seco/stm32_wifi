b71f7406c82e988535fec2eb27b0d4fe7065816a
zzes <kuanglong0312@sina.com>
2017-04-21 09:06:30 +0800

    主程序

---
+++ ContanaControl/ContanaControl/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace ContanaControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+            port.Open();
+            port.Write("F");
+            port.Close();
+        }
+    }
+}
